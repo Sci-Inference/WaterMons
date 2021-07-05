cfaeb61b98c9f9ea0010a1c4873290bcc306384a
Allen Chiang <49981062+allen-chiang@users.noreply.github.com>
2021-07-05 16:19:07 +0800

    update

--- water_mons/performance/utils.py
+++ water_mons/performance/utils.py
@@ -2,27 +2,41 @@ import numpy as np
 from typing import Optional
 
 
+def _timeSeries_check(timeSeries:np.array):
+    if len(timeSeries) <= 1:
+        raise ValueError("timeSeries Data should have at least two items")
+    if (timeSeries < 1).any():
+        raise ValueError("timeSeries items should be greater than 0")
+
+
 def max_drawn(timeSeries:np.array)-> np.array:
-    minData = timeSeries.min()
-    return (minData - timeSeries[0])/timeSeries[0]
+    _timeSeries_check(timeSeries)
+    minData = timeSeries[-1]
+    maxData = timeSeries[:-1].max()
+    return (maxData - minData)/maxData
 
 
 def percent_return(timeSeries:np.array)-> np.array:
+    _timeSeries_check(timeSeries)
     return (timeSeries[-1] - timeSeries[0])/timeSeries[0]
 
 
 def number_return(timeSeries:np.array)-> np.array:
+    _timeSeries_check(timeSeries)
     return timeSeries[-1] - timeSeries[0]
 
 
 def std(timeSeries:np.array)-> np.array:
+    _timeSeries_check(timeSeries)
     return timeSeries.std()
 
 
 def sharpe_ratio(timeSeries:np.array, riskFree: Optional[int] = 0) -> np.array:
+    _timeSeries_check(timeSeries)
     return (number_return(timeSeries = timeSeries) - riskFree)/std(timeSeries=timeSeries)
 
 def sortino_ratio(timeSeries:np.array, riskFree: Optional[int] = 0)->np.array:
+    _timeSeries_check(timeSeries)
     downSideStd = (timeSeries - timeSeries[0])
     downSideStd = downSideStd[downSideStd < 0].std()
     return (number_return(timeSeries = timeSeries) - riskFree)/downSideStd
--- water_mons/test/test_performance.py
+++ water_mons/test/test_performance.py
@@ -1,2 +1,62 @@
-from watermons.performance import utils
 import pandas as pd
+import numpy as np
+from water_mons.performance.utils import *
+import pytest
+
+@pytest.fixture
+def data_input():
+    return np.array([11,21,13,100,10,10])
+
+@pytest.fixture
+def data_wrong_input():
+    return {
+        'neg':np.array([-1,2,1]),
+        'len':np.array([0])
+        }
+
+class Test_Performance_Utils:
+    
+    def test_max_drawn(self,data_input,data_wrong_input):
+        neg = data_wrong_input['neg']
+        lenData = data_wrong_input['len']
+        with pytest.raises(ValueError):
+            max_drawn(neg)
+            max_drawn(lenData)
+        data = data_input
+        res = max_drawn(data)
+        assert res ==0.9
+
+    def test_percent_return(self,data_input,data_wrong_input):
+        neg = data_wrong_input['neg']
+        lenData = data_wrong_input['len']
+        with pytest.raises(ValueError):
+            percent_return(neg)
+            percent_return(lenData)
+
+    def test_number_return(self,data_input,data_wrong_input):
+        neg = data_wrong_input['neg']
+        lenData = data_wrong_input['len']
+        with pytest.raises(ValueError):
+            number_return(neg)
+            number_return(lenData)
+
+    def test_std(self,data_input,data_wrong_input):
+        neg = data_wrong_input['neg']
+        lenData = data_wrong_input['len']
+        with pytest.raises(ValueError):
+            std(neg)
+            std(lenData)
+
+    def test_sharpe_ratio(self,data_input,data_wrong_input):
+        neg = data_wrong_input['neg']
+        lenData = data_wrong_input['len']
+        with pytest.raises(ValueError):
+            sharpe_ratio(neg)
+            sharpe_ratio(lenData)
+
+    def test_sortino_ratio(self,data_input,data_wrong_input):
+        neg = data_wrong_input['neg']
+        lenData = data_wrong_input['len']
+        with pytest.raises(ValueError):
+            sortino_ratio(neg)
+            sortino_ratio(lenData)
