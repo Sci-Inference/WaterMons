1ce7fb97df260ce29e9b2c2e6978e57481cdf35a
Allen Chiang <49981062+allen-chiang@users.noreply.github.com>
2021-07-12 15:24:43 +0800

    router init

--- water_mons/test/test_connection.py
+++ water_mons/test/test_connection.py
@@ -1,5 +1,15 @@
 import pytest
 import numpy as np
 import pandas as pd
+import os
 from water_mons.performance.utils import *
-from water_mons.performance.performance import PerformanceBase
+from water_mons.connection.sqlalchemy_connector import DBConnector
+
+
+
+class Test_DBConnector:
+
+    def test_declare_schema(self):
+        dbc = DBConnector("sqlite:///./water-mons.sqllite")
+        dbc.declare_schema()
+        os.remove('./water-mons.sqllite')
