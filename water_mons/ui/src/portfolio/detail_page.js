9166119936546aa7db5d6814302ae39bc4b55c95
Allen Chiang <49981062+allen-chiang@users.noreply.github.com>
2021-07-13 18:37:08 +0800

    landing draft v0

---
+++ water_mons/ui/src/portfolio/detail_page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles, withStyles, createTheme, ThemeProvider,createMuiTheme } from '@material-ui/core/styles';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Data_Table from '../utils/data_table';
+
+
+
--- water_mons/ui/src/portfolio/landing_page.js
+++ water_mons/ui/src/portfolio/landing_page.js
@@ -1,84 +1,94 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import { makeStyles, withStyles, createTheme, ThemeProvider,createMuiTheme } from '@material-ui/core/styles';
+import { makeStyles, withStyles, createTheme, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Data_Table from '../utils/data_table';
-
+import Box from '@material-ui/core/Box';
 
 
 
 class Portfolio_Landing_Page extends React.Component {
-    constructor(props) {
-        super(props);
-        this.create_table = this.create_table.bind(this);
-        this.theme = createMuiTheme({
-            palette: {
-              primary: {
-                main: '#E9E9E9',
-                contrastText: '#A5A5A5'
-              }
-            },
-          });
-        this.columns = [
-            {
-              field: 'name',
-              headerName: 'Name',
-              width: 150,
-              editable: true,
-            },
-            {
-              field: 'description',
-              headerName: 'Description',
-              width: 300,
-              editable: true,
-            },
-            {
-              field: 'create',
-              headerName: 'Create',
-              type: 'date',
-              width: 150,
-              editable: true,
-            }
-          ];
+  constructor(props) {
+    super(props);
+    this.create_table = this.create_table.bind(this);
+    this.theme = createMuiTheme({
+      palette: {
+        primary: {
+          main: '#E9E9E9',
+          contrastText: '#A5A5A5'
+        },
+      },
+    });
 
-        this.rows = [
-            { id:1,name: 'Industry', description: 'Industry Leader', create: '2021-01-02' },
-            { id:2,name: 'Technical', description: 'Tenical Indicator Selection', create: '2021-01-01' },
-          ];
-    }
+    this.styleClass = makeStyles({
+      root: {
+        backgroundColor: "red",
+        margin: "10"
+      }
+    });
 
-    create_table(rows,columns) {
-        return (
-            Data_Table(rows,columns)
-        )
-    }
+    this.columns = [
+      {
+        field: 'name',
+        headerName: 'Name',
+        width: 150,
+        editable: true,
+      },
+      {
+        field: 'description',
+        headerName: 'Description',
+        width: 300,
+        editable: true,
+      },
+      {
+        field: 'create',
+        headerName: 'Create',
+        type: 'date',
+        width: 150,
+        editable: true,
+      }
+    ];
 
-    render() {
-        return (
-            <div>
-                <Container>
-                    <Grid container spacing={12}>
-                        <Grid item xs={1}>
-                            <ThemeProvider theme={this.theme}>
-                                <Button
-                                    variant="contained"
-                                    color={"primary"}
-                                    startIcon={<AddCircleOutlineIcon />}
-                                >
-                                    Create
-                                </Button>
-                            </ThemeProvider>
-                        </Grid>
-                        <Grid item xs={12} alignContent={'center'}>
-                            {this.create_table(this.rows,this.columns)}
-                        </Grid>
-                    </Grid>
-                </Container>
-            </div>
-        )
-    }
+    this.rows = [
+      { id: 1, name: 'Industry', description: 'Industry Leader', create: '2021-01-02' },
+      { id: 2, name: 'Technical', description: 'Tenical Indicator Selection', create: '2021-01-01' },
+    ];
+  }
+
+  create_table(rows, columns) {
+    return (
+      Data_Table(rows, columns)
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Grid container spacing={12}>
+            <Grid item xs={1} className={this.styleClass.root}>
+              <ThemeProvider theme={this.theme}>
+                <Box pb={5}>
+                  <Button
+                    variant="contained"
+                    color={"primary"}
+                    startIcon={<AddCircleOutlineIcon />}
+                  >
+                    Create
+                  </Button>
+                </Box>
+              </ThemeProvider>
+            </Grid>
+            <Grid item xs={12} alignContent={'center'}>
+              {this.create_table(this.rows, this.columns)}
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default Portfolio_Landing_Page
