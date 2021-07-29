c738d02a24e55131388329135884343039b8ebb5
Allen Chiang <49981062+allen-chiang@users.noreply.github.com>
2021-07-29 15:14:23 +0800

    portfolio

--- water_mons/performance/portfolio.py
+++ water_mons/performance/portfolio.py
@@ -1,6 +1,4 @@
 import pandas as pd
-from collections import defaultdict
-
 from pandas.core.indexing import convert_from_missing_indexer_tuple
 from water_mons.connection.online_stock_connector import StockConnector
 import numpy as np
@@ -19,9 +17,20 @@ class Portfolio(object):
             'date':createDate,
             'option':option,
             'price':price,
-            'Close':StockConnector(self.conStr).get_data(createDate,createDate).Close.tolist()[-1],
+            'Close':StockConnector(ticker,self.conStr).get_data(createDate,createDate).Close.tolist()[-1],
             'number':number
         })
+        
+    def pad_tickers(self):
+        df = pd.DataFrame(self.tickerList)
+        dateList = df['date'].unique().tolist()
+        tickerList = df['ticker'].unique().tolist()
+        for i in tickerList:
+            tmp = df[df.ticker == i]
+            tmpDateList = tmp['date'].unique().tolist()
+            for j in dateList:
+                if j not in tmpDateList:
+                    self.append_ticker(i,j,None,None,0)
 
 
     def stock_value(self,data):
@@ -29,41 +38,66 @@ class Portfolio(object):
         purchaseValue = 0
         purchaseNum = 0
         for i in data:
+            if len(res)<1 and i['option'] is None:
+                continue
             if i['option'] == 'buy':
                 purchaseNum += i['number']
                 pvalue = i['Close'] * purchaseNum
                 purchaseValue += i['price'] * i['number']
-                res.append({'date':i['date'],'value':pvalue,'earn':np.nan})
+                earn = -1*i['price'] * i['number']
+                res.append({'date':i['date'],'value':pvalue,'earn':earn})
             if i['option'] == 'sell':
-                earn = (i['price'] * i['number']) - (purchaseValue/purchaseNum)
+                earn = (i['price'] * i['number'])
                 purchaseNum -= i['number']
                 if purchaseNum < 0:
                     raise
                 pvalue = i['Close'] * purchaseNum
                 res.append({'date':i['date'],'value':pvalue,'earn':earn})
+            if i['option'] == None:
+                pvalue = i['Close'] * purchaseNum
+                res.append({'date':i['date'],'value':pvalue,'earn':0})
         return res
 
-    def _merge_portfolio_values(self,a,b):
-        for i in a:
-            if i in b:
-                for j in a[i]:
-                    a[i][j] +=b[i][j]
-                continue
+    def _merge_portfolio_values(self,a,b,holding):
+        sameDate = False
+        cacheDate = None
         for i in b:
-            if i not in a:
-                a[i] = b[i]
-        return a
+            if i['date'] == cacheDate:
+                sameDate = True
+            if i['date'] in a:
+                prevp = a[i['date']]['portfolio_value']
+            else:
+                prevp = 0
+            if (np.abs(holding) <= np.abs(i['earn'])) and  (i['earn'] < 0):
+                print('buy')
+                print(f"{holding} {i['earn']} {holding}")
+                a[i['date']] = {'portfolio_value':prevp+i['value'],'holding':holding}
+            elif (np.abs(holding)>= np.abs(i['earn'])) and (i['earn'] < 0) and sameDate:
+                print('same')
+                holding = holding + i['earn']
+                a[i['date']] = {'portfolio_value':prevp+i['value'],'holding':holding}
+            else:
+                print('sell')
+                print(f"{holding} {i['earn']} {holding + i['earn']}")
+                holding = holding +  + i['earn']
+                a[i['date']] ={'portfolio_value':prevp+i['value'],'holding':holding}
+            cacheDate = i['date']
+        return a,holding
 
     def create_portfolio(self):
         portValue = None
         df = pd.DataFrame(self.tickerList)
+        holding = 0
+        portValue = {}
         for i in df.ticker.unique():
-            data = df[df['ticker'] == i].to_dict('records')
+            data = df[df['ticker'] == i].sort_values('date').to_dict('records')
             stock = self.stock_value(data)
             if portValue is None:
                 portValue = copy.deepcopy(stock)
                 continue
-            portValue = self._merge_portfolio_values(portValue,stock)
+            portValue,h = self._merge_portfolio_values(portValue,stock,holding)
+            holding = h
         return portValue
+
 
     
