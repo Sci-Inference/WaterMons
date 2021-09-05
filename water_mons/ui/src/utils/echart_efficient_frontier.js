ae494702015e1660bc436e35c2bcd1521cd39bdd
Allen Chiang <49981062+allen-chiang@users.noreply.github.com>
2021-09-05 15:33:35 +0800

    update

--- water_mons/ui/src/utils/echart_efficient_frontier.js
+++ water_mons/ui/src/utils/echart_efficient_frontier.js
@@ -20,52 +20,73 @@ class Efficient_Frontier extends React.Component {
   }
 
   async getFronterData() {
-    let data = await fetch(
-      "http://localhost:5000/risk/efficientFrontier",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            'tickers':this.props.tickerList,
-            'method':this.props.method,
-            'startDate':this.props.startDate,
-            'endDate':this.props.endDate}),
-      }
-    ).then((d) => d.json());;
+    let data = await fetch("http://localhost:5000/risk/efficientFrontier", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        tickers: this.props.tickerList,
+        method: this.props.method,
+        startDate: this.props.startDate,
+        endDate: this.props.endDate,
+      }),
+    }).then((d) => d.json());
     console.log(data);
-    return data
+    return data;
   }
 
   draw(data) {
     console.log(data);
     let option = {
       tooltip: {
-        trigger: 'axis'
-    },
+        trigger: "axis",
+      },
       xAxis: {
-          min:Math.min(...data['lineStd']),
-          type:'value',
-        },
-      yAxis: {
-        min:Math.min(...data['lineRe']),
+        min: Math.min(...data["lineStd"]),
         type: "value",
       },
-      series: [{
-        type:'line',
-        data:data['frontierLine'],
-        markPoint: {
-          data: [
-            {name: 'optimal_point', value: 'optimal', xAxis: data['pav'], yAxis: data['par']}
-          ]
+      yAxis: {
+        min: Math.min(...data["lineRe"]),
+        type: "value",
       },
-      markLine: {
-        data: [
-            {type: 'average', name: '平均值'}
-        ]
-    }
-        }],
+      grid: {top: '40%'},
+      series: [
+        {
+          type: "line",
+          data: data["frontierLine"],
+          markPoint: {
+            data: [
+              {
+                name: "optimal_point",
+                value: "optimal",
+                xAxis: data["pav"],
+                yAxis: data["par"],
+              },
+            ],
+          },
+        },
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: '25%',
+          center: ['50%', '25%'],
+          data: [
+              {value: 1048, name: '搜索引擎'},
+              {value: 735, name: '直接访问'},
+              {value: 580, name: '邮件营销'},
+              {value: 484, name: '联盟广告'},
+              {value: 300, name: '视频广告'}
+          ],
+          // emphasis: {
+          //     itemStyle: {
+          //         shadowBlur: 10,
+          //         shadowOffsetX: 0,
+          //         shadowColor: 'rgba(0, 0, 0, 0.5)'
+          //     }
+          // }
+      }
+      ],
     };
     console.log(option);
     this.eChartsRef.current
